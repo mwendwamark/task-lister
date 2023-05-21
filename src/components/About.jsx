42514b01251ffb3ab3c8d47be5c7f9d5bbd8f98c
mark mwendwa <mwendwamark310@gmail.com>
2023-05-21 21:16:16 +0300

    add the necessary css files

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+// import React from "react";
+// import "./About.css";
+
+// function About() {
+//   return (
+//     <div>
+//       <header>
+//         <h1 className="about-h1">Task-Lister</h1>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default About;
